304f96e6c6951bc694739cdc9f0c8fb7189c20e4
triaki-nabil-prog <nabil.triaki2@gmail.com>
2023-01-26 22:03:13 +0100

    created and tested two functions  reverseString and calculator

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ export function cap(string) {
     array[0] = array[0].toUpperCase();
     return array.join('');
 }
+
+export function rev(string) {
+    return string.split("").reverse().join('');
+}
+
+export const calc = {
+    add: (a, b) => { return a + b; },
+    sub: (a, b) => { return a - b; },
+    div: (a, b) => { return a / b; },
+    mul: (a, b) => { return a * b; },
+}
--- app.test.js
+++ app.test.js
@@ -1,5 +1,16 @@
-import{cap} from "./app"
+import { cap, rev, calc } from "./app"
 
-test('capitalization', ()=>{
-expect(cap("AbCd")).toBe("Abcd");
+test('capitalization', () => {
+    expect(cap("AbCd")).toBe("Abcd");
+});
+
+test('reverseString', () => {
+    expect(rev("abcd")).toBe("dcba");
+})
+
+test('calculator', () =>{
+expect(calc.add(2,2)).toBe(4);
+expect(calc.sub(2,2)).toBe(0);
+expect(calc.div(4,2)).toBe(2);
+expect(calc.mul(2,3)).toBe(6);
 });
